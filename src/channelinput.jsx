77dc67c657672a299a8b3dfce83e3521608c16a0
bhawya lohia <bhawya.lohia45@gmail.com>
2021-02-22 02:10:21 +0530

    set heading and input

--- src/channelinput.jsx
+++ src/channelinput.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 function ChannelInput()
 {
- return (<div>
-
+ return (<div className="channelinput"> 
+        <div>
+         <h3>Enter the youtube channel id <br/>(you can find it at the end of the url of the channel page).</h3>
+         <input type="text" className="form-control"></input>
+         <br/>
+         <button className="btn btn-primary btn-block">Get Stats</button>
+        </div>
  </div>);
 }
 export default ChannelInput;
--- src/heading.jsx
+++ src/heading.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 function Heading()
 {
-    return (<div>
-
+    return (<div className="heading">
+        <h1>You<span style={{color:"red"}}>Tube</span> Channel Statistics</h1>
         </div>);
 }
 export default Heading;
