5e7e68d410d9cbd2b8fe7cce129e11769b85608c
bhawya lohia <bhawya.lohia45@gmail.com>
2021-03-02 02:11:44 +0530

    add hover effects

--- src/chart.jsx
+++ src/chart.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import DataBox from "./DataBox";
 function Chart(props)
 {
+    const paddingTop=100;
+    const paddingLeft=350;
     const {data,height,width}=props;
+    const [hoverEffects,setHoverEffects]=useState({x:0,y:0,enable:false});
     function getX(index)
     {
         return ((width)*((index+1)/getMaxX()));
@@ -32,6 +36,45 @@ function Chart(props)
         return ("M "+x+" "+y+" ");
         else return ("L "+x+" "+y+" ");
     }
+    function getNearestElementPos(presX)
+    {
+        var minDis= Math.abs(getX(0)-presX);
+        var nearestIndex=0;
+        for(var i=0;i<data.length;i++)
+        {
+            if(Math.abs(getX(i)-presX)<minDis)
+            {
+                minDis=Math.abs(getX(i)-presX);
+                nearestIndex=i;
+            }
+        }
+        return nearestIndex;
+    }
+    function handleMouseMove(event)
+    {
+        var {offsetX,offsetY}=event.nativeEvent;
+        setHoverEffects({x:offsetX-paddingLeft,y:offsetY-paddingTop,enable:true});
+    }
+    function handleMouseLeave()
+    {
+        setHoverEffects({x:0,y:0,enable:false});
+    }
+    function createCircle(x,y)
+    {
+        var pos=getNearestElementPos(x);
+        var svgX=Math.floor(getX(pos));
+        var svgY=Math.floor(getY(parseInt(data[pos].statistics.viewCount)));
+        return (<g>
+             <circle cx={svgX} cy={svgY} r="5" stroke="black" stroke-width="3" fill="red" />
+        </g>);
+    }
+    function createDataBox(x,y)
+    {
+        var pos=getNearestElementPos(x);
+        var svgX=Math.floor(getX(pos));
+        var svgY=Math.floor(getY(parseInt(data[pos].statistics.viewCount)));
+        return (<DataBox svgX={svgX} svgY={svgY} data={data[pos]}></DataBox>);
+    }
     function makePath()
     {
         if(data.length==0)
@@ -41,7 +84,7 @@ function Chart(props)
         path1=path1.toString();
         console.log(path1);
         console.log(data);
-        return <path d={path1} className="lineChartPath"/>
+        return <path d={path1} className="lineChartPath" />
     }
     function makeAxis()
     {
@@ -64,16 +107,18 @@ function Chart(props)
         path1=(data.map(getPoint));
         path1=path1.toString();
         path1+=("L "+xAxis.xRight+" "+xAxis.yRight+" L "+xAxis.xLeft+" "+xAxis.yLeft+" ");
-        return <path d={path1} className="lineChartArea"/>
+        return <path d={path1} className="lineChartArea" />
     }
    return (<div className="chartDivision">
-       <svg width="1000" height="700">
-       <svg x="350" y="100">
+       <svg width="1000" height="700" >
+       <svg x={paddingLeft} y={paddingTop} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
         {makePath()}
         {makeAxis()}
-        {/* {makeAreaFilled()} */}
+        {makeAreaFilled()}
+        {hoverEffects.enable&&(createCircle(hoverEffects.x,hoverEffects.y))}
        </svg>
        </svg>
+       {hoverEffects.enable&&(createDataBox(hoverEffects.x,hoverEffects.y))}
    </div>);
 }
 export default Chart;
